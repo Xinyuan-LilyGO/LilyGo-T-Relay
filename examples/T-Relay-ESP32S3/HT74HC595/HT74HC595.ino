3ed4273d498760f7ce5e141b1f48aa03740f2355
lewisxhe <lewishe@outlook.com>
2024-10-04 11:55:14 +0800

    Update relay test examples

--- examples/T-Relay-ESP32S3/HT74HC595/HT74HC595.ino
+++ examples/T-Relay-ESP32S3/HT74HC595/HT74HC595.ino
@@ -19,11 +19,14 @@ void setup()
     Serial.begin(115200);
     Serial.println("Ciallo");
 
+    Serial.println("Power on and set the 74HC595 to output disable mode");
     pinMode(HT74HC595_OUT_EN, OUTPUT);
     digitalWrite(HT74HC595_OUT_EN, HIGH); // 关闭输出数据 HT74HC595
 
+    Serial.println("Set all relay outputs to low level");
     HT74HC595->setAllLow();
 
+    Serial.println("Set GPIO4 to low level to enable relay output");
     digitalWrite(HT74HC595_OUT_EN, LOW);
 }
 
@@ -31,30 +34,40 @@ void loop()
 {
     Serial.printf("System running time: %d\n\n", (uint32_t)millis() / 1000);
 
+    Serial.println("All Channel turn on.");
     HT74HC595->setAllHigh();
     delay(1000);
+    Serial.println("All Channel turn off.");
     HT74HC595->setAllLow();
     delay(1000);
+    Serial.println("Relay(CH1) turn on");
     HT74HC595->set(0, HIGH, true);
     delay(1000);
+    Serial.println("Relay(CH2) turn on");
     HT74HC595->set(0, LOW, true);
     HT74HC595->set(1, HIGH, true);
     delay(1000);
+    Serial.println("Relay(CH3) turn on");
     HT74HC595->set(1, LOW, true);
     HT74HC595->set(2, HIGH, true);
     delay(1000);
+    Serial.println("Relay(CH4) turn on");
     HT74HC595->set(2, LOW, true);
     HT74HC595->set(3, HIGH, true);
     delay(1000);
+    Serial.println("Relay(CH5) turn on");
     HT74HC595->set(3, LOW, true);
     HT74HC595->set(4, HIGH, true);
     delay(1000);
+    Serial.println("Relay(CH6) turn on");
     HT74HC595->set(4, LOW, true);
     HT74HC595->set(5, HIGH, true);
     delay(1000);
+    Serial.println("LED (CH7) turn on");
     HT74HC595->set(5, LOW, true);
     HT74HC595->set(6, HIGH, true);
     delay(1000);
+    Serial.println("LED (CH8) turn on");
     HT74HC595->set(6, LOW, true);
     HT74HC595->set(7, HIGH, true);
     delay(1000);
